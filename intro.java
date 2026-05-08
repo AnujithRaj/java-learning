38147f6c42f0b626f4bd87d40df4c2ede14a928c
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-05-08 19:04:44 +0530

    Initial Java setup

---
+++ intro.java
@@ -0,0 +1,21 @@
+//  Some of the biggest organization of the world are built using Java.
+// Java is very easy to learn and implement
+// Java has a very strong community support, so no natter what doubts you have, you'll easily b able to find answers to all of them.
+// There are many integrated improvement conditions(IDE's) in Java that gives different offices to programming advancement to developers. Increadible java IDE's such as Eclipse, NettBeans, IntelliJ IDEA, etc. play a major job in the accomplishment of Java.
+
+public class intro {
+    public static void main(String args[]) {
+        // Output
+        System.out.println("Hello World!");
+
+
+        // variables
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+
+        int multi = a * b;
+        System.out.println(multi);
+    }
+}
---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+        System.out.println("Java is easy to learn");
+        System.out.println("Strong community support");
+        System.out.println("Widely used in big organizations");
+
+    }
+}
