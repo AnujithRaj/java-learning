307ab1d6a32a70758ecbab1b9d44f19b51f673a2
Anujith <249439266+AnujithRaj@users.noreply.github.com>
2026-05-17 17:55:35 +0530

    Added Nested If-Else In Java

---
+++ basics/NestedIfElse.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class NestedIfElse {
+    public static void main(String[] args) {
+
+       // nested If-Else
+       int num =8;
+       if (num >= 5) {
+        if (num > 6) {
+            System.out.println("Big Number");
+        }
+        else {
+            System.out.println("Number is either 5 or 6");
+        }
+       }
+
+    }
+}
